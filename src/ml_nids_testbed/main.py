1044808f2d105fde882b64ce2be065ebf2879243
dawetmaster <andika.naufal48@gmail.com>
2024-05-24 21:18:21 +0700

    start implement parsing subcommands and delete old files

--- src/ml_nids_testbed/main.py
+++ src/ml_nids_testbed/main.py
@@ -15,18 +15,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    
+    if args.subprogram == "boil_the_frog":
         pass
+    elif args.subprogram == "mix_pcap":
+        pass
+    else:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-    # boil_the_frog.boil_the_frog_linear_rps_constant_bytes("10.5.0.2", 80, min_rps=3000, max_rps=80000, increment_size=100)
-    # smuggler.smuggle_packets(
-    #     "10.5.0.2", 
-    #     default_port=80,
-    #     many_protocol_types=True, 
-    #     other_packet_type=[ProtocolType.UDP, ProtocolType.TCP], 
-    #     smuggled_packet_type=ProtocolType.HTTP, 
-    #     smuggled_packet_probability=0.05, 
-    #     src_ip_range="10.5.0.0/24", 
-    #     rps=300, 
-    #     time_limit=60
-    #     )
     logger.info("Okay")
--- src/ml_nids_testbed/utils/cli.py
+++ src/ml_nids_testbed/utils/cli.py
@@ -13,4 +13,33 @@ def setup_cli_parser():
         action="store_true",
         dest="verbose",
     )
+    subprograms = parser.add_subparsers(dest="subprogram")
+    boil_the_frog_parser = subprograms.add_subparsers("boil_the_frog")
+    boil_the_frog_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+    )
+    mix_pcap_parser = subprograms.add_subparsers("mix_pcap")
+    mix_pcap_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+    )
+    mix_pcap_parser.add_argument(
+        "-p",
+        "--packet",
+        action="append",
+        dest="packet",
+        help="Packet type to mix",
+        choices=["TCP", "UDP", "ICMP"],
+    )
+    mix_pcap_parser.add_argument(
+        "--pcap-file",
+        action="append",
+        dest="pcap_file",
+        help="Pcap file to mix",
+    )
     return parser
